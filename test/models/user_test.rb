c677dce47e4e42625e9c9b341c1b54e20494bba6
aditya010497 <aditya010497@gmail.com>
2021-09-14 06:59:30 +0530

    Add user microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -54,8 +54,15 @@ test "email addresses should be saved as lower-case" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
     end
-    test "password should have a minimum length" do
+  test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
     end
+  test "associated microposts should be destroyed" do
+      @user.save
+      @user.microposts.create!(content: "Lorem ipsum")
+      assert_difference 'Micropost.count', -1 do
+      @user.destroy
+      end
+      end
     end
